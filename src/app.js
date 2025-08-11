03f65eaf8fd87fbeba6d54c169fbc812a617f827
Lorena9208 <lore.0492@hotmail.com>
2025-08-11 10:39:54 +0000

    Complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
+  const domain = ['.es', '.com', '.org', '.net'];
+
+  for (const itemPronoun of pronoun) {
+    for (const itemAdj of adj) {
+      for (const itemNoun of noun) {
+        for (const itemDomain of domain) {
+          console.log(`${itemPronoun}${itemAdj}${itemNoun}${itemDomain}`);
+        }
+      }
+    }
+  }
+}
